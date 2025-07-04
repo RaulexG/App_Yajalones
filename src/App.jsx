b6d66562b35fa49088f18548b200f4e4a82cea83
Raulrex <raulrex@gmail.com>
2025-07-03 21:41:07 -0600

    chore: estructura inicial de carpetas y archivos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Styles/tailwind.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl text-red-400 font-bold mt-40 text-center">
+        Hello world!
+        <br />
+        yajalon-app
+      </h1>
+    </>
+  )
+}
+
+export default App
